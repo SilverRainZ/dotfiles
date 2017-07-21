0c44e65351bff9d62c145338d27672cdf6f1cba3
Shengyu Zhang <silverrainz@outlook.com>
2017-07-22 01:30:04 +0800

    Add .config/, remove .ssh/ and nichijou update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/sh
 
-files=$(ls -A1 | egrep -v 'README.md|.gitignore|.git|deploy.sh')
+files=$(ls -A1 | egrep -v 'README.md|deploy.sh|.gitignore|.git$|.config')
 target=~
 
 link(){
     ln -sfv "$PWD/$1" "$2"
 }
 
+# Link home
+files=$(ls -A1 | egrep -v 'README.md|deploy.sh|.gitignore|.git$|.config')
+target=~
+for f in $files; do
+    link "$f" "$target"
+done
+
+# Link xdg config home
+files=$(ls -A1 .config)
+target=~/.config
 for f in $files; do
     link "$f" "$target"
 done

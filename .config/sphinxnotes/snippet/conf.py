a63ddc3e1e413bee16e661b4edc81212dbced7f1
Shengyu Zhang <i@silverrainz.me>
2022-01-24 17:15:17 +0800

    Rename private sphinx document

--- .config/sphinxnotes/snippet/conf.py
+++ .config/sphinxnotes/snippet/conf.py
@@ -1,5 +1,4 @@
 base_urls = {
     '银色子弹': 'file:///home/la/documents/bullet/_build/html/',
-    '银子弹子': 'file:///home/la/documents/tellub/_build/html/',
-    'Srain': 'file:///home/la/git/srain-docs/_build/html/',
+    '浪人泊处': 'file:///home/la/documents/ronin/_build/html/',
 }

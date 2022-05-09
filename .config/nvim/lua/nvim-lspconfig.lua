8e9f8477f984aa5be98cb5f85c6a019dc2fd0292
Shengyu Zhang <zhangshengyu.0@bytedance.com>
2022-05-09 12:04:00 +0800

    nvim: Remove default config

--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -38,9 +38,5 @@ local servers = { 'pyright', 'rust_analyzer', 'gopls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
   }
 end

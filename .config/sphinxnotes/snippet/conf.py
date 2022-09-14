af2f64a3549821d3bb5e0e35a1b1dbe59902c7fa
Shengyu Zhang <i@silverrainz.me>
2022-09-14 19:13:44 +0800

    Add services doc

--- .config/sphinxnotes/snippet/conf.py
+++ .config/sphinxnotes/snippet/conf.py
@@ -1,4 +1,5 @@
 base_urls = {
     '银色子弹': 'file:///home/la/documents/bullet/_build/html/',
     '浪人泊处': 'file:///home/la/documents/ronin/_build/html/',
+    'services': 'file:///home/la/documents/services/_build/html/',
 }

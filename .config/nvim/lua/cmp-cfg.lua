559c06b7ff514ba6b7f2ae16e31f6e55ec2c0cbe
Shengyu Zhang <i@silverrainz.me>
2022-11-18 23:39:26 +0800

    nvim: adopt to neovim's breaking changes
    https://github.com/neovim/nvim-lspconfig/issues/1891

--- .config/nvim/lua/cmp-cfg.lua
+++ .config/nvim/lua/cmp-cfg.lua
@@ -1,6 +1,5 @@
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities
 
 -- nvim-cmp setup
 local cmp = require 'cmp'

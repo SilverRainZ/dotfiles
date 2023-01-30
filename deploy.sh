2f049b7f066073f6401be80f49a6979075c7a613
Shengyu Zhang <zhangshengyu.0@bytedance.com>
2023-01-30 21:21:50 +0800

    deploy: Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-files=$(ls -A1 | egrep -v 'README.md|deploy.sh|.gitignore|.git$|.config')
-target=~
-
 link(){
     ln -sfv "$PWD/$1" "$2"
 }
@@ -11,12 +8,15 @@ link(){
 files=$(ls -A1 | egrep -v 'README.rst|deploy.sh|.gitignore|.git$|.config|Makefile|conf.py|index.rst')
 target=~
 for f in $files; do
+    unlink "$target" || true
     link "$f" "$target"
 done
 
 # Link xdg config home
 files=$(ls -A1 .config)
 target=~/.config
+mkdir -p $target || true
 for f in $files; do
-    link ".config/$f" "$target"
+    unlink "$target/$f" || true
+    link ".config/$f" "$target/$f"
 done

832b62cf24dda769277074ba0ff0dfbeec689d93
Shengyu Zhang <i@silverrainz.me>
2023-07-02 11:04:53 +0800

    sway: Re-org workspaces; Use swaync instead of mako

--- .config/sway/multi-monitors.sh
+++ .config/sway/multi-monitors.sh
@@ -11,16 +11,20 @@ clamshell_mode() {
 }
 
 outputs=$(swaymsg --pretty --type get_outputs)
+echo $outputs
+
 
 if echo "$outputs" | grep "$OUT_BENQ"; then
-    echo 1234
+    echo Output: $OUT_BENQ
     swaymsg "output '$OUT_BENQ' scale 1.5 pos 0 0"
     swaymsg "output '$OUT_BUILTIN' scale 2.5 pos 0 1440"
     clamshell_mode "$OUT_BUILTIN"
 elif echo "$outputs" | grep "$OUT_DELL"; then
+    echo Output: $OUT_DELL
     swaymsg "output '$OUT_BUILTIN' scale 2.5 pos 0 0"
     swaymsg "output '$OUT_DELL' scale 1.5 pos 2560 0" # 3860/1.5 = 2560
     clamshell_mode "$OUT_BUILTIN"
 else
+    echo Output: ALL
     swaymsg "output * scale 2.2"
 fi

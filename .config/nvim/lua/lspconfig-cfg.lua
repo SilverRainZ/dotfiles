063ae84a06d53a44f06276d201a2053b94b7a3eb
Shengyu Zhang <zhangshengyu.0@bytedance.com>
2024-03-17 14:21:36 +0800

    nvim: New LSP server esbonio

--- .config/nvim/lua/lspconfig-cfg.lua
+++ .config/nvim/lua/lspconfig-cfg.lua
@@ -53,7 +53,10 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'rust_analyzer', 'gopls' }
+--
+-- esbonio: pip install esbonio
+-- gopls: go install golang.org/x/tools/gopls@latest
+local servers = { 'pyright', 'rust_analyzer', 'gopls', 'esbonio' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,

22daac378504cb0862aef4d57b56437ae47ccacd
Shengyu Zhang <i@silverrainz.me>
2025-07-02 11:02:55 +0800

    Nichijou update

--- .config/sphinxnotes/snippet/conf.py
+++ .config/sphinxnotes/snippet/conf.py
@@ -1,4 +1,5 @@
 base_urls = {
     # Serve by SystemD user service: sphinx-bullet-autobuild.service
     '银色子弹': 'http://127.0.0.1:30500/',
+    '张盛宇的简历': 'file:///home/la/documents/resume/_build/html/',
 }

8515ac2f44f6b948eec1570a38bef026c898ec68
Shengyu Zhang <i@silverrainz.me>
2026-03-25 19:35:52 +0800

    opencode: Update

--- .config/nvim/lua/opencode-cfg.lua
+++ .config/nvim/lua/opencode-cfg.lua
@@ -1,29 +1,37 @@
-vim.g.opencode_opts = {
-  -- Your configuration, if any — see `lua/opencode/config.lua`, or 'goto definition' on the type or field.
-  provider = {
-    -- See also simple-daemon@opencode.service
-    -- cmd = 'opencode attach http://localhost:44047',
-    enabled = "tmux",
-    tmux = {
-      -- ...
-    }
-  }
+local opencode_cmd = 'opencode --port'
 
+---@type snacks.terminal.Opts
+local snacks_terminal_opts = {
+  win = {
+    position = 'right',
+    enter = false,
+    on_win = function(win)
+      -- Set up keymaps and cleanup for an arbitrary terminal
+      require('opencode.terminal').setup(win.win)
+    end,
+  },
+}
+
+---@type opencode.Opts
+vim.g.opencode_opts = {
+  server = {
+    start = function()
+      require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+    end,
+    stop = function()
+      require('snacks.terminal').get(opencode_cmd, snacks_terminal_opts):close()
+    end,
+    toggle = function()
+      require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
+    end,
+  },
 }
 
 -- Required for `opts.events.reload`.
 vim.o.autoread = true
 
 -- Recommended/example keymaps.
-vim.keymap.set({ 'n' }, '<leader>oo', function() require('opencode').toggle() end,                          { desc = 'Toggle opencode' })
+vim.keymap.set({ 'n' },      '<leader>oo', function() require('opencode').toggle() end,                          { desc = 'Toggle opencode' })
 vim.keymap.set({ 'n', 'x' }, '<leader>oa', function() require('opencode').ask('@this: ', { submit = true }) end, { desc = 'Ask opencode…' })
 vim.keymap.set({ 'n', 'x' }, '<leader>ox', function() require('opencode').select() end,                          { desc = 'Execute opencode action…' })
-
-vim.keymap.set({ 'n', 'x' }, '<leader>oy',  function() return require('opencode').operator('@this ') end,        { desc = 'Add range to opencode', expr = true })
-
-vim.keymap.set('n', '<S-C-u>', function() require('opencode').command('session.half.page.up') end,   { desc = 'Scroll opencode up' })
-vim.keymap.set('n', '<S-C-d>', function() require('opencode').command('session.half.page.down') end, { desc = 'Scroll opencode down' })
-
--- You may want these if you stick with the opinionated '<C-a>' and '<C-x>' above — otherwise consider '<leader>o…'.
-vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment under cursor', noremap = true })
-vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement under cursor', noremap = true })
+vim.keymap.set({ 'n', 'x' }, '<leader>oy', function() return require('opencode').operator('@this ') end,         { desc = 'Add range to opencode', expr = true })
